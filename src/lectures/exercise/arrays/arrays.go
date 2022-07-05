fdb0cf63848876dde34313e3f0a1e661d014249c
Richard Claydon <git@icdw.co.uk>
2022-07-05 09:56:59 -0400

    Arrays

--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,6 +18,51 @@ package main
 
 import "fmt"
 
+type Product struct {
+	name  string
+	price float64
+}
+
+func totals(items [4]Product) (int, float64) {
+	var total = 0.0
+	var count = 0
+
+	for i := 0; i < len(items); i++ {
+
+		if items[i].name != "" {
+			total += items[i].price
+			count += 1
+		}
+	}
+
+	return count, total
+}
+
+func lastItemName(items [4]Product) string {
+	return items[len(items)-1].name
+}
+
+func printStats(items [4]Product) {
+
+	count, total := totals(items)
+
+	fmt.Println("List total is", total)
+	fmt.Println("Item Count is", count)
+	fmt.Println("Last item", lastItemName(items))
+}
+
 func main() {
 
+	items := [...]Product{
+		{name: "Pencil", price: 2.99},
+		{name: "Paper", price: 5.34},
+		{name: "Pen", price: 14.23},
+		{name: "Ink", price: 1.32},
+	}
+
+	printStats(items)
+
+	items[3] = Product{name: "Goat", price: 2.99}
+
+	printStats(items)
 }
