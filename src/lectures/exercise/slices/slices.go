0c522321993e41bbb7fa77925d574ea2c79ede94
Richard Claydon <git@icdw.co.uk>
2022-07-08 08:34:00 -0400

    slices tutorial

--- src/lectures/exercise/slices/slices.go
+++ src/lectures/exercise/slices/slices.go
@@ -18,5 +18,31 @@ import "fmt"
 
 type Part string
 
+func printAssemblyLine(parts []Part) {
+	fmt.Println("---")
+	for i := 0; i < len(parts); i++ {
+		part := parts[i]
+		fmt.Println(part)
+	}
+}
+
 func main() {
+
+	// Create an assembly line having any three parts
+	assemblyLine := make([]Part, 3)
+	assemblyLine[0] = "Part 1"
+	assemblyLine[1] = "Part 2"
+	assemblyLine[2] = "Part 3"
+
+	printAssemblyLine(assemblyLine)
+
+	// Add two new parts to the line
+	assemblyLine = append(assemblyLine, "Part 4", "Part 5")
+
+	printAssemblyLine(assemblyLine)
+
+	// Slice the assembly line so it contains only the two new parts
+	assemblyLine = assemblyLine[3:5]
+	printAssemblyLine(assemblyLine)
+
 }
