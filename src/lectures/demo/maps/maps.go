5c381349409a6629eed23b66392bbcc449bed5d6
Richard Claydon <git@icdw.co.uk>
2022-07-08 09:17:02 -0400

    maps tutorial

--- src/lectures/demo/maps/maps.go
+++ src/lectures/demo/maps/maps.go
@@ -3,4 +3,28 @@ package main
 import "fmt"
 
 func main() {
+	shoppingList := make(map[string]int)
+	shoppingList["eggs"] = 11
+	shoppingList["bread"] = 1
+	shoppingList["milk"] += 1
+	shoppingList["eggs"] += 1
+
+	fmt.Println(shoppingList)
+
+	delete(shoppingList, "milk")
+	fmt.Println(shoppingList)
+
+	fmt.Println("We need", shoppingList["eggs"], "eggs")
+
+	cereal, found := shoppingList["cereal"]
+	if !found {
+		fmt.Println("Cereal:", cereal)
+	}
+
+	var totalItems = 0
+	for _, amount := range shoppingList {
+		totalItems += amount
+	}
+
+	fmt.Println("Total items:", totalItems)
 }
--- src/lectures/exercise/maps/maps.go
+++ src/lectures/exercise/maps/maps.go
@@ -29,6 +29,71 @@ const (
 	Retired     = 3
 )
 
+func statusLabel(status int) string {
+	var statusLabel = ""
+	switch status {
+	case Online:
+		statusLabel = "Online"
+	case Offline:
+		statusLabel = "Offline"
+	case Maintenance:
+		statusLabel = "Maintenance"
+	case Retired:
+		statusLabel = "Retired"
+	}
+	return statusLabel
+}
+
+//* Create a function to print server status displaying:
+//  - number of servers
+//  - number of servers for each status (Online, Offline, Maintenance, Retired)
+
+func showServerStatus(servers map[string]int) {
+	fmt.Println("\nThere are ", len(servers), "servers")
+	fmt.Println("---")
+
+	statuses := map[int]int{
+		Online:      0,
+		Offline:     0,
+		Maintenance: 0,
+		Retired:     0,
+	}
+
+	for _, status := range servers {
+		statuses[status]++
+	}
+
+	for status, element := range statuses {
+		statusLabel := statusLabel(status)
+
+		fmt.Println(statusLabel, ":", element)
+	}
+}
+
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+
+	serverMap := make(map[string]int, len(servers))
+
+	for i := 0; i < len(servers); i++ {
+		serverMap[servers[i]] = Online
+	}
+
+	fmt.Println(serverMap)
+
+	//  - call display server info function
+	showServerStatus(serverMap)
+	//  - change server status of `darkstar` to `Retired`
+	serverMap["darkstar"] = Retired
+	//  - change server status of `aiur` to `Offline`
+	serverMap["aiur"] = Offline
+	//  - call display server info function
+	showServerStatus(serverMap)
+	//  - change server status of all servers to `Maintenance`
+	for server, _ := range serverMap {
+		serverMap[server] = Maintenance
+	}
+	//  - call display server info function
+	showServerStatus(serverMap)
+
 }
