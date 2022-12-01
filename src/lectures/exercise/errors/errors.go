8d10041329341ed94a3a544b8aa6949029b268e0
Richard Claydon <richard.claydon@koho.ca>
2022-12-01 08:49:43 -0500

    Errors exersise

--- src/lectures/exercise/errors/errors.go
+++ src/lectures/exercise/errors/errors.go
@@ -15,3 +15,63 @@
 //* Use the `errors` package to generate errors
 
 package timeparse
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Time struct {
+	Hour   int
+	Minute int
+	Second int
+}
+
+type TimeParseError struct {
+	msg   string
+	input string
+}
+
+func (t *TimeParseError) Error() string {
+	return fmt.Sprintf("%v: %v", t.msg, t.input)
+}
+
+func parseTime(input string) (Time, error) {
+
+	parts := strings.Split(input, ":")
+
+	if len(parts) != 3 {
+		return Time{}, &TimeParseError{"invalid time format", input}
+	}
+
+	hour, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return Time{}, &TimeParseError{fmt.Sprintf("invalid hour input: %v", err), input}
+	}
+
+	if hour < 0 || hour > 24 {
+		return Time{}, &TimeParseError{fmt.Sprintf("invalid hour input: %v", err), input}
+	}
+
+	minutes, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return Time{}, &TimeParseError{fmt.Sprintf("invalid hour input: %v", err), input}
+	}
+
+	if minutes < 0 || minutes > 59 {
+		return Time{}, &TimeParseError{fmt.Sprintf("invalid minutes input: %v", err), input}
+	}
+
+	seconds, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return Time{}, &TimeParseError{fmt.Sprintf("invalid seconds input: %v", err), input}
+	}
+
+	if seconds < 0 || seconds > 59 {
+		return Time{}, &TimeParseError{fmt.Sprintf("invalid seconds input: %v", err), input}
+	}
+
+	return Time{hour, minutes, seconds}, nil
+
+}
---
+++ src/lectures/exercise/errors/errors_test.go
@@ -0,0 +1,26 @@
+package timeparse
+
+import "testing"
+
+func TestParseTime(t *testing.T) {
+	table := []struct {
+		time string
+		ok   bool
+	}{
+		{"19:00:12", true},
+		{"1:3:44", true},
+		{"bad", false},
+		{"1:-3:44", false},
+		{"0:59:59", true},
+		{"", false},
+		{"aa:bb:cc", false},
+		{"5:23:", false},
+	}
+
+	for _, data := range table {
+		_, err := parseTime(data.time)
+		if data.ok && err != nil {
+			t.Errorf("%v: %v, error should be nil", data.time, err)
+		}
+	}
+}
