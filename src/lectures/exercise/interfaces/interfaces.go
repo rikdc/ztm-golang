3bd3688c08486f3f94fe413f9136acf0ecf1a9fa
Richard Claydon <richard.claydon@koho.ca>
2022-12-01 08:17:20 -0500

    Interfaces exercise

--- src/lectures/exercise/interfaces/interfaces.go
+++ src/lectures/exercise/interfaces/interfaces.go
@@ -21,6 +21,61 @@ package main
 
 import "fmt"
 
-func main() {
+const (
+	SmallLift = iota
+	StandardLift
+	LargeLift
+)
 
+type Lift int
+
+type LiftPicker interface {
+	PickLift() Lift
+}
+
+type Car string
+type Motorcycle string
+type Truck string
+
+func (v Car) String() string {
+	return fmt.Sprintf("Car: %v", string(v))
+}
+
+func (d Car) PickLift() Lift {
+	return StandardLift
+}
+
+func (v Motorcycle) String() string {
+	return fmt.Sprintf("Motorcycle: %v", string(v))
+}
+
+func (d Motorcycle) PickLift() Lift {
+	return SmallLift
+}
+
+func (v Truck) String() string {
+	return fmt.Sprintf("Truck: %v", string(v))
+}
+
+func (d Truck) PickLift() Lift {
+	return LargeLift
+}
+
+func directVehicle(p LiftPicker) {
+	captions := make(map[Lift]string, 2)
+	captions[SmallLift] = "Small"
+	captions[StandardLift] = "Standard"
+	captions[LargeLift] = "Large"
+
+	fmt.Printf("send %v to %s lift\n", p, captions[p.PickLift()])
+}
+
+func main() {
+	c := Car("Honda Civic")
+	t := Truck("Ford F150")
+	m := Motorcycle("A Motorcycle")
+
+	directVehicle(c)
+	directVehicle(t)
+	directVehicle(m)
 }
