4cb2a71387e79ae4af84433a3ba672ab67695316
Richard Claydon <richard.claydon@koho.ca>
2023-09-11 09:57:07 -0400

    Adding code for reader exercise

--- src/lectures/exercise/reader/reader.go
+++ src/lectures/exercise/reader/reader.go
@@ -18,8 +18,63 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+const (
+	CommandHello  = "hello"
+	CommandBye    = "bye"
+	StatsLines    = "lines"
+	StatsCommands = "commands"
+)
 
 func main() {
+	stats := map[string]int{
+		"lines":    0,
+		"commands": 0,
+	}
 
+	r := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("Enter a command (hello, bye, Q or q): ")
+		input, inputErr := r.ReadString('\n') // Alternative would be to use .Scan()
+		if inputErr != nil {
+			fmt.Printf("Error reading input: %v\n", inputErr)
+			continue
+		}
+
+		command := strings.TrimSpace(input)
+
+		// Exit conditions
+		if strings.ToLower(command) == "q" {
+			break
+		}
+		if inputErr == io.EOF {
+			break
+		}
+
+		stats[StatsLines]++
+
+		// Command handling
+		switch command {
+		case CommandHello:
+			fmt.Println("Response: Hello!")
+			stats[StatsCommands]++
+		case CommandBye:
+			fmt.Println("Response: Bye!")
+			stats[StatsCommands]++
+		default:
+			fmt.Println("Unknown command, true again!")
+		}
+
+	}
+
+	fmt.Printf("Number of commands: %d\n", stats["commands"])
+	fmt.Printf("Number of lines: %d\n", stats["lines"])
 }
