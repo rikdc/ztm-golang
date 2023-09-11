a0d85543ab68e7cdcdc909eb6c106ff180f6ebbc
Richard Claydon <richard.claydon@koho.ca>
2023-09-11 14:56:50 -0400

    lecture for literals

--- src/lectures/demo/func-literal/func-literal.go
+++ src/lectures/demo/func-literal/func-literal.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
-func main() {
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
 
+func compute(lhs, rhs int, op func(int, int) int) int {
+	fmt.Printf("Running a computation with %v & %v\n", lhs, rhs)
+	return op(lhs, rhs)
+}
+
+func main() {
+	fmt.Println("10 + 2 =", compute(10, 2, add))
 }
