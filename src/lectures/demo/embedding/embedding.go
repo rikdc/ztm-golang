f6be11fbcf50fdaa2e4cc9bb3dc473571dff098c
Richard Claydon <richard.claydon@koho.ca>
2023-09-11 10:12:09 -0400

    Completed demo for embedding

--- src/lectures/demo/embedding/embedding.go
+++ src/lectures/demo/embedding/embedding.go
@@ -2,6 +2,73 @@ package main
 
 import "fmt"
 
-func main() {
+const (
+	Small = iota
+	Medium
+	Large
+)
 
+const (
+	Ground = iota
+	Air
+)
+
+type BeltSize int
+type Shipping int
+
+func (b BeltSize) String() string {
+	return [...]string{"Small", "Medium", "Large"}[b]
+}
+
+func (s Shipping) String() string {
+	return [...]string{"Ground", "Air"}[s]
+}
+
+type Conveyor interface {
+	Convey() BeltSize
+}
+
+type Shipper interface {
+	Ship() Shipping
+}
+
+type WarehouseAutomator interface {
+	Conveyor
+	Shipper
+}
+
+type SpamMail struct {
+	amount int
+}
+
+func (s SpamMail) String() string {
+	return "Spam Mail"
+}
+
+func (s *SpamMail) Ship() Shipping {
+	return Air
+}
+
+func (s *SpamMail) Convey() BeltSize {
+	return Small
+}
+
+type ToxicWaste struct {
+	weight int
+}
+
+func (t *ToxicWaste) Ship() Shipping {
+	return Ground
+}
+
+func automate(item WarehouseAutomator) {
+	fmt.Printf("Convey %v on %v conveyor\n", item, item.Convey())
+}
+
+func main() {
+	mail := SpamMail{amount: 40000}
+	automate(&mail)
+
+	//waste := ToxicWaste{weight: 300}
+	//automate(&waste)
 }
