4e8cf6f0b5b15bd6d4cf93cd56172a59d2f7af60
Richard Claydon <richard.claydon@koho.ca>
2023-09-18 14:02:23 -0400

    completed waitgroup lecture

--- src/lectures/demo/waitgroups/waitgroups.go
+++ src/lectures/demo/waitgroups/waitgroups.go
@@ -1,11 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "sync"
-    "time"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	var wg sync.WaitGroup
+
+	counter := 0
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		counter += 1
+
+		go func() {
+			defer func() {
+				fmt.Println(counter, "go routines remaining")
+				counter -= 1
+				wg.Done()
+			}()
+			duration := time.Duration(rand.Intn(500) * int(time.Millisecond))
+
+			fmt.Println("Sleeping for", duration)
+			time.Sleep(duration)
+		}()
+
+		fmt.Println("waiting for goroutines to finish")
+		wg.Wait()
+
+		fmt.Println("done!")
+	}
 }
